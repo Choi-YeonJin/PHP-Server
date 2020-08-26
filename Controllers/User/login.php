4f6820805b1122375c1de72b23b7ec99465b187a
Choi-Yeonjin <yeonjin9721@naver.com>
2020-08-26 15:47:12 +0900

    [Update] retrun value is json

--- Controllers/User/login.php
+++ Controllers/User/login.php
@@ -6,8 +6,8 @@ include_once ("../../application/lib/autoload.php");
 
 $lineStr = "<br><br>---------------------------------------------------<br><br>";
 // Tip.... 에러를 확인하고 싶을 경우 사용 -> https://ra2kstar.tistory.com/102 확인
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
 // 1. 파라미터 값 객체에 넣기
 // ex) [http://itkoo.site?id=1&name=구지원] 으로 요청했을 경우
@@ -16,23 +16,25 @@ $userModel->setByArray(json_decode(file_get_contents('php://input'))); // 요청
 $userModel->setCreatedAt(time()); // 시간은 서버 시간으로 세팅
 
 // 2. 객체를 json 타입으로 return
-echo "<p>1) 파라미터를 객체에 set, 객체를 json 형태로 return</p><br>";
-echo $userModel->getJson(new UserModel());
-echo $lineStr;
+//echo "<p>1) 파라미터를 객체에 set, 객체를 json 형태로 return</p><br>";
+//echo $userModel->getJson(new UserModel());
+//echo $lineStr;
 
 
 // 3. CRUD
 $userDAO = new UserDAO();
 
-echo "<p>3) Login, 단일 검색</p><br>";
 $myid = $userModel->getMyid();
 $password = $userModel->getPassword();
 $result = $userDAO->select($myid,$password); // id로 단일 검색
 if($result){
-    echo "로그인 성공";
-}else {
-    echo "로그인 실패";
-}
+    $data = ["result" => "true"];
 
-echo $lineStr;
+    echo json_encode($data);
+
+}else {
+    $data = ["result" => "false"];
+
+    echo json_encode($data);
+}
 ?>
--- Controllers/User/signUp.php
+++ Controllers/User/signUp.php
@@ -7,8 +7,8 @@ include_once ("../../application/lib/autoload.php");
 
 $lineStr = "<br><br>---------------------------------------------------<br><br>";
 // Tip.... 에러를 확인하고 싶을 경우 사용 -> https://ra2kstar.tistory.com/102 확인
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
 // 1. 파라미터 값 객체에 넣기
 // ex) [http://itkoo.site?id=1&name=구지원] 으로 요청했을 경우
@@ -17,16 +17,16 @@ $userModel->setByArray(json_decode(file_get_contents('php://input'))); // 요청
 $userModel->setCreatedAt(time()); // 시간은 서버 시간으로 세팅
 
 // 2. 객체를 json 타입으로 return
-echo "<p>1) 파라미터를 객체에 set, 객체를 json 형태로 return</p><br>";
-echo $userModel->getJson(new UserModel());
-echo $lineStr;
+//echo "<p>1) 파라미터를 객체에 set, 객체를 json 형태로 return</p><br>";
+//echo $userModel->getJson(new UserModel());
+//echo $lineStr;
 
 
 // 3. CRUD
 $userDAO = new UserDAO();
 
 // 3-1. INSERT
-echo "<p>1) SignUp, 회원가입 </p><br>";
+echo "1) SignUp, 회원가입 <br>";
 $myid=$userModel->getMyid();
 $password=$userModel->getPassword();
 $name=$userModel->getName();
@@ -37,7 +37,7 @@ $result = $userDAO->selectByID($myid);
 // echo $result."<br>";
 if($result==0){
     $userId = $userDAO->insert($userModel); // 위에 받았던 (파라미터->객체) insert
-    echo "userId : {$userId}".$lineStr;
+    echo "userId : {$userId}";
 }else{
     echo "ID is already taken";
 }
