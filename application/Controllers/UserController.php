8ff6c0dac5699ed43b407b574e1b7a69be8096d5
Choi-Yeonjin <yeonjin9721@naver.com>
2020-09-01 17:48:15 +0900

    login, signUp Controller

--- application/Controllers/UserController.php
+++ application/Controllers/UserController.php
@@ -1,29 +1,76 @@
 <?php
 
 namespace Controllers;
+
 use Model\UserModel;
 use DAO\UserDAO;
 
-include_once("application/lib/autoload.php");
+include_once("../../application/lib/autoload.php");
 
 class UserController
 {
-
-    public function create(){
+    public function create($data)
+    {
         $userModel = new UserModel();
         $userDAO = new UserDAO();
-        $userModel->setByArray(json_decode(file_get_contents('php://input'))); // 요청받은 파라미터를 객체에 맞게끔 변형, data set
+        $userModel->setByArray(json_decode($data)); // 요청받은 파라미터를 객체에 맞게끔 변형, data set
         $userModel->setCreatedAt(time()); // 시간은 서버 시간으로 세팅
-        echo "create";
+        $userModel->setUpdatedAt(time()); // 시간은 서버 시간으로 세팅
+
+        $myid = $userModel->getMyid();
+
+        //name 중복검사
+        $result = $userDAO->selectmyidByID($myid);
+
+        if ($result == 0) {
+            $userId = $userDAO->insert($userModel); // 위에 받았던 (파라미터->객체) insert
+
+            $data = ["result" => "true",
+                "userId" => "{$userId}"];
+
+            echo json_encode($data);
+        } else {
+            $data = ["result" => "false",
+                "errorMessage" => "ID is alreay taken"];
+
+            echo json_encode($data);
+        }
     }
 
-    public function update($userModel){
+    public function login($data)
+    {
+        $userModel = new UserModel();
+        $userModel->setByArray(json_decode($data)); // 요청받은 파라미터를 객체에 맞게끔 변형, data set
+        $userModel->setCreatedAt(time()); // 시간은 서버 시간으로 세팅
+        $userModel->setUpdatedAt(time()); // 시간은 서버 시간으로 세팅
+
+        $userDAO = new UserDAO();
+
+        $myid = $userModel->getMyid();
+        $password = $userModel->getPassword();
+        $result = $userDAO->select($myid, $password); // id로 단일 검색
+        if ($result) {
+            $data = ["result" => "true"];
+
+            echo json_encode($data);
+        } else {
+            $data = ["result" => "false"];
+
+            echo json_encode($data);
+        }
     }
 
-    public function select($userId){
+    public function update($userModel)
+    {
     }
 
-    public function delete($userId){
+    public function select($uriArray)
+    {
+        echo $uriArray[2];
+    }
+
+    public function delete($userId)
+    {
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -1,18 +1,27 @@
 <?php
 
+use Controllers\UserController;
+
+include_once ("../../application/lib/autoload.php");
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 $method = $_SERVER["REQUEST_METHOD"];
 $uri = $_SERVER["REQUEST_URI"];
 $uriArray = explode("/", $uri);
 
+$userController = new UserController();
 
 switch ($method . ":" . $uriArray[1]) {
     case "GET:user":
-        echo "user get";
-        // userController->select($uriArray);
+        $userController->select($uriArray);
         break;
     case "POST:user":
-        // userController->insert(file_get_contents ....);
+        $userController->create(file_get_contents('php://input'));
         break;
+    case "POST:login":
+        $userController->login(file_get_contents('php://input'));
     default:
         break;
 }
